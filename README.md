0569b717b037c57e7696209cfc7a2e1d4a257d0e
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-04-02 20:07:49 +0200

    initial project version

---
+++ README.md
@@ -0,0 +1,2 @@
+# faceDetection
+This repo aims to detect faces from some samples of images get from yelp challenge 
---
+++ groupsFaceDetection.py
@@ -0,0 +1,83 @@
+# Working !!!!!!!!!!!!!!!!!!!!
+# This file loads all face images then detect faces from them
+# needed packages
+import cv2
+# load images 
+# Returns images loaded in stack and gray version of the image in stack too
+def loadfaces():
+	#Images main path 
+	path= "/home/rana/Desktop/yelp/faceDetection/people/"
+	#creat list with images names
+	imgnames=[  "408830.jpg" ,"409268.jpg" ,"469954.jpg"  ,
+				"470265.jpg" ,"470580.jpg" ,"470804.jpg" ,
+				"470294.jpg" ,"470647.jpg" ,"470822.jpg" ,
+				"470408.jpg" ,"470662.jpg" ,"470894.jpg" ,
+				"470953.jpg" ,"471344.jpg" ,"471271.jpg" ,
+				"471073.jpg" ,"471463.jpg" ,"471279.jpg" ,
+				"471103.jpg" ,"471550.jpg" ,"471333.jpg" ,
+				"471671.jpg" ,"471753.jpg" ,"471797.jpg" ,
+				"471800.jpg" ]
+	#create empty list for images loaded 
+	imgstack =[]
+	grayStack =[]
+	# Loop to load the images in the path  
+	for i in imgnames:
+		# Read the image
+		image = cv2.imread(path + i)
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		imgstack.append(image)
+		grayStack.append(gray)
+		# display last element in the list
+		#print (i)
+		#cv2.imshow("Faces found" ,imgstack[-1])
+		#cv2.waitKey(0)	
+	return imgstack , grayStack
+# face detection function
+# input : 
+#--------
+# fi : target imges stack 
+# gi : target images stack in gray scale
+# =====================
+# output:
+# -------
+# display imges with green fram in the detected imges
+# Helper link :
+# https://realpython.com/blog/python/face-recognition-with-python/
+def findFacaes(fi,gi):
+	#
+	cascPath = "/home/rana/Desktop/yelp/faceDetection/FaceDetect/haarcascade_frontalface_default.xml"
+	# Create the haar cascade
+	faceCascade = cv2.CascadeClassifier(cascPath)
+	# Loop on the images stack
+	#for i,item in enumerate (gi):
+	# Detect faces in the image
+	i = 15
+	faces = faceCascade.detectMultiScale(
+	    gi[15],
+	    scaleFactor= 1.1,
+	    minNeighbors= 3,
+	    minSize=(15, 15),
+	    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+	print "Found {0} faces!".format(len(faces))
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		if (w < 53):
+	    		cv2.rectangle(fi[i], (x, y), (x+w, y+h), (0, 255, 0), 2)
+	    		print(" x :",x)
+	    		print(" y :",y)
+	    		print(" w :",w)
+	    		print(" h :",h)
+			cv2.imshow("Faces found" ,fi[i])
+			cv2.imwrite("face {0}.png".format(i),fi[i])
+			cv2.waitKey(0)
+	
+
+# main function
+def main():	
+	# load face imgs
+	faceImgs , grayfaces = loadfaces()
+	# detect faces
+	detectedFaces = findFacaes(faceImgs,grayfaces)
+
+main()
